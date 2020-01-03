18c95dadec32766834b1531cefe9956a544a3724
ranguifeng <136627209@qq.com>
2020-01-03 10:28:41 +0800

    修改登录验证页面

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,14 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">欢迎您！</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    @if(auth()->guest())
+                        请先登录您的应用
+                    @else
+                        您已经登录了！
+                    @endif
                 </div>
             </div>
         </div>
