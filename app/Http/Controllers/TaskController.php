1ce0e170c9897f283c7e01044803845fb45fcc20
ranguifeng <136627209@qq.com>
2020-01-08 17:16:25 +0800

    1

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
     public function index()
     {
-
+        $lists = User::withTrashed()->where('age', '>=', '22')->where(['name' => 'lisi'])->get();
+        dump($lists);
     }
 }
